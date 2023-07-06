78b0be598445952dea4e08d3e3398dd4f98c45ce
Aris0506 <sinopaaris@gmail.com>
2023-07-06 20:14:46 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// Memuat kode untuk membuat, mengonfigurasi, dan menjalankan server HTTP menggunakan Hapi.
+const Hapi = require('@hapi/hapi');
+const routes = require('./src/route');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        // chrome.exe --user-data-dir="C://Chrome dev session" --disable-web-security
+        origin: ['*'],
+      },
+    },
+  });
+  //  ini dari const router
+  server.route(routes);
+  // asyc ya pasti ada await
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
